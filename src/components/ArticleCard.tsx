28e65107c950b004ca41dd11361ae717aae5b105
mahmoud <mahmoud@kali.vvv>
2024-07-13 13:06:01 -0500

    finished cards

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -2,11 +2,11 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { Article } from '@/lib/types'; 
-import { EllipsisVertical } from 'lucide-react';
+import { EllipsisVertical, ExternalLink, WandSparkles } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import {motion} from "framer-motion"
-
+import Link from "next/link";
 interface ArticleProps {
   article: Article;
 }
@@ -17,45 +17,73 @@ const ArticleCard: React.FC<ArticleProps> = ({ article }) => {
 
   return (
     <div className="flex flex-col min-w-[380px] w-full flex-1 md:max-w-[800px] h-[400px] border rounded shadow-md">
+        
         <motion.div
-        initial={{ opacity: 1 }}
-        whileHover={{ opacity: 0.5 }}
-        transition={{ duration: 0.3 }}
-        className="rounded-sm mb-2 object-cover h-full w-full"
-      >
-        <Image
-          src={article.thumbnail}
-          alt={article.title}
-          width={400}
-          height={150}
-          className="h-full w-full object-cover"
-        />
-      </motion.div>
+  className="relative h-full w-full"
+  initial="initial"
+  whileHover="hover"
+  variants={{
+    initial: { opacity: 1 },
+    hover: { opacity: 1 },
+  }}
+>
+  {/* Image container */}
+  <motion.div
+    className="h-full w-full"
+    variants={{
+      initial: { opacity: 1 },
+      hover: { opacity: .5 },
+    }}
+    transition={{ duration: 0.3 }}
+  >
+    <Image
+      src={article.thumbnail}
+      alt={article.title}
+      width={400}
+      height={150}
+      className="h-full w-full object-cover"
+    />
+  </motion.div>
 
-      {/* Add two buttons that will pop up on hover */}
-      <motion.div
-        initial={{ y: 20, opacity: 0 }}
-        whileHover={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.3 }}
-        className="absolute z-50 inset-0 flex flex-col items-center justify-center"
-      >
+  {/* Overlay that appears on hover */}
+  <motion.div
+    className="absolute inset-0 bg-black bg-opacity-75 flex flex-col items-center gap-4 justify-center"
+    variants={{
+      initial: { opacity: 0 },
+      hover: { opacity: 1 },
+    }}
+    transition={{ duration: 0.3 }}
+  >
+          <Link href={`/dashboard/articles/${article.title.replaceAll(" ", "-")}`} passHref 
+          rel="noopener noreferrer">
         <motion.button
-          initial={{ x: -100, opacity: 0 }}
-          whileHover={{ x: 0, opacity: 1 }}
+          className="bg-fuchsia-400 flex gap-2 items-center justify-center px-8 py-4 text-xl text-black bg-opacity-75 hover:bg-opacity-100  rounded shadow"
+          variants={{
+            initial: { x: -100, opacity: 0 },
+            hover: { x: 0, opacity: 1 },
+          }}
           transition={{ duration: 0.3 }}
-          className="bg-white bg-opacity-75 hover:bg-opacity-100 p-2 rounded shadow mb-2"
         >
-          Button 1
+          Summarized with AI <WandSparkles/>
         </motion.button>
+      </Link>
+      <Link href={article.link} passHref target="_blank"
+          rel="noopener noreferrer">
         <motion.button
-          initial={{ x: 100, opacity: 0 }}
-          whileHover={{ x: 0, opacity: 1 }}
+          className="bg-white flex gap-2 items-center justify-center px-8 py-4 text-xl text-black bg-opacity-75 hover:bg-opacity-100  rounded shadow"
+          variants={{
+            initial: { x: 100, opacity: 0 },
+            hover: { x: 0, opacity: 1 },
+          }}
           transition={{ duration: 0.3 }}
-          className="bg-white bg-opacity-75 hover:bg-opacity-100 p-2 rounded shadow"
         >
-          Button 2
-        </motion.button>
-      </motion.div>
+          Read original article <ExternalLink/>
+          </motion.button>
+      </Link>
+    </motion.div>
+  </motion.div>
+
+
       
       <div className='flex-col  gap-2 p-2 flex '>
       <h2 className="text-lg ml-1 font-semibold">
--- src/components/StreamCard.tsx
+++ src/components/StreamCard.tsx
@@ -2,8 +2,10 @@
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { Article, Stream } from '@/lib/types'; 
-import { EllipsisVertical } from 'lucide-react';
+import { EllipsisVertical, ExternalLink, WandSparkles } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 
 interface ArticleProps {
@@ -17,14 +19,74 @@ const StreamCard: React.FC<ArticleProps> = ({ stream }) => {
   return (
     <div className="flex flex-col min-w-[380px] w-full flex-1 md:max-w-[800px] h-[400px] border rounded shadow-md">
        
-       
-       <Image
-        src={stream.thumbnail_url.replace("-{width}x{height}", "")}
-        alt={stream.title}
-        width={400}
-        height={150}
-        className="rounded-sm hover:opacity-50 mb-2 object-cover h-full w-full"
-      />
+      
+
+
+<motion.div
+  className="relative h-full w-full"
+  initial="initial"
+  whileHover="hover"
+  variants={{
+    initial: { opacity: 1 },
+    hover: { opacity: 1 },
+  }}
+>
+  {/* Image container */}
+  <motion.div
+    className="h-full w-full"
+    variants={{
+      initial: { opacity: 1 },
+      hover: { opacity: .5 },
+    }}
+    transition={{ duration: 0.3 }}
+  >
+    <Image
+      src={stream.thumbnail_url.replace("-{width}x{height}", "")}
+      alt={stream.title}
+      width={400}
+      height={150}
+      className="h-full w-full object-cover"
+    />
+  </motion.div>
+
+  {/* Overlay that appears on hover */}
+  <motion.div
+    className="absolute inset-0 bg-black bg-opacity-75 flex flex-col items-center gap-4 justify-center"
+    variants={{
+      initial: { opacity: 0 },
+      hover: { opacity: 1 },
+    }}
+    transition={{ duration: 0.3 }}
+  >
+          <Link href={`/dashboard/streams/${stream.title.replaceAll(" ", "-")}`} passHref 
+          rel="noopener noreferrer">
+        <motion.button
+          className="bg-fuchsia-400 flex gap-2 items-center justify-center px-8 py-4 text-xl text-black bg-opacity-75 hover:bg-opacity-100  rounded shadow"
+          variants={{
+            initial: { x: -100, opacity: 0 },
+            hover: { x: 0, opacity: 1 },
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          Summarized with AI <WandSparkles/>
+        </motion.button>
+      </Link>
+      <Link href={stream.thumbnail_url} passHref target="_blank"
+          rel="noopener noreferrer">
+        <motion.button
+          className="bg-white flex gap-2 items-center justify-center px-8 py-4 text-xl text-black bg-opacity-75 hover:bg-opacity-100  rounded shadow"
+          variants={{
+            initial: { x: 100, opacity: 0 },
+            hover: { x: 0, opacity: 1 },
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          Read original article <ExternalLink/>
+          </motion.button>
+      </Link>
+    </motion.div>
+  </motion.div>
+
       
       <div className='flex-col  gap-2 p-2 flex '>
       <h2 className="text-lg ml-1 font-semibold">
